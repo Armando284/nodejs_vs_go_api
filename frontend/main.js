f40b05ef210ada9f1253e77962bec3c2023201e8
Armando Peña Tamayo (Ender) <ender@aleph.engineering>
2022-11-23 10:48:52 -0500

    Refactoring frontend and adding go api

--- frontend/main.js
+++ frontend/main.js
@@ -1,29 +1,30 @@
-function debounce(fn, delay) {
+function debounce(fn, delay, port, resultComponent) {
   let timer;
-  return (...args) => {
+  return () => {
     if (timer) clearTimeout(timer);
     timer = setTimeout(() => {
-      fn.apply(this, ...args);
+      fn(port, resultComponent);
     }, delay);
   }
 }
 
-async function testNodeApi() {
-  console.log('fn run!')
+async function testApi(port, resultComponent) {
   const times = [];
-  for (let i = 0; i < 2; i++) {
+  for (let i = 0; i < 10; i++) {
     const tax = (Math.random() * 10).toFixed(2);
     const start = performance.now();
-    const response = await axios.get(`http://localhost:3000/api/get_products_with_taxes/${tax}`);
-    console.log(response);
+    const response = await axios.get(`http://localhost:${port}/api/get_products_with_taxes/${tax}`);
     const end = performance.now();
     times.push(end - start);
-    // console.log(times);
   }
   const totalTime = times.reduce((a, b) => a + b, 0);
-  nodeResult.innerText = `${totalTime / times.length}ms`;
+  resultComponent.innerText = `${totalTime / times.length}ms`;
 }
 
 const nodeTestButton = document.getElementById('node-test-button');
 const nodeResult = document.getElementById('node-result');
-nodeTestButton.addEventListener('click', debounce(testNodeApi, 500));
+nodeTestButton.addEventListener('click', debounce(testApi, 500, 3000, nodeResult));
+
+const goTestButton = document.getElementById('go-test-button');
+const goResult = document.getElementById('go-result');
+goTestButton.addEventListener('click', debounce(testApi, 500, 9010, goResult));
